d8fd26350a1fce17ff9a920b765cc35dcf01ff8f
Boochoo <Boochoo@users.noreply.github.com>
2017-11-10 16:32:50 +0200

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
+import {GeolocationService} from "./geolocation.service";
+import {DataService} from "./data.service";
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
 
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MdButtonModule, MdIconModule, MdInputModule, MdSelectModule, MdSliderModule,
+  MdToolbarModule, MdCardModule, MdSlideToggleModule} from "@angular/material";
+import {'hammerjs'};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, BrowserAnimationsModule,MdButtonModule, MdIconModule,
+    MdInputModule, MdSelectModule, MdSliderModule,
+    MdToolbarModule, MdCardModule, MdSlideToggleModule
   ],
-  providers: [],
+  providers: [GeolocationService, DataService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
